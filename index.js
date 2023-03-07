1f8305186196ecf7c7ec3c4d9e57d0194d8f6e90
akhil3110 <akhil1659@gmail.com>
2023-03-07 17:51:22 +0530

    Updated Task Files

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var shell = require('shelljs');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const creds = require("./pass.json")
-
+const prompt=require("prompt-sync")({sigint:true}); 
+const date= new Date();
 
 const getAuth = async (id) => {
     try {
@@ -15,18 +16,38 @@ const getAuth = async (id) => {
     }
   }
 
+
 // Initialize the sheet - doc ID is the long id in the sheets URL
 const main =async() =>{
     try {
         const doc = await getAuth("1df0PbQJJVPO9qXI9w9gVlqX02rErPHxJ2IVk2GnokfQ")
         const d=await doc.loadInfo();
-        console.log(doc.title);
         const sheet = doc.sheetsByIndex[0];
-        const rows = await sheet.getRows(); 
-        for (key in rows) {
-            console.log(key)
-           shell.exec(`${rows[key][sheet.headerValues[0]]}`)
+        const rows = await sheet.getRows();
+
+        await sheet.loadCells('A1:E100');
+        
+        var ch = 'y';
+        while(ch=='y' || ch=='Y'){
+          var command = prompt("Enter Command to run ");
+          for (key in rows) {
+              if(rows[key][sheet.headerValues[1]]== command){
+                if(rows[key][sheet.headerValues[2]]=='n' || rows[key][sheet.headerValues[2]]=='N' ){
+                  shell.exec(`${rows[key][sheet.headerValues[0]]}`)
+                  var num = +key;
+                  const run_status = sheet.getCell(num+1,2);
+                  run_status.value='y';
+                  const date_field = sheet.getCell(num+1,4);
+                  date_field.value=`${date}`;
+                  await sheet.saveUpdatedCells();
+                }else{
+                  console.log("Command Already ececuted")
+                }
+              } 
+          }
+          ch=prompt("Do you want to run more commands ")
         }
+        
     } catch (error) {
         console.log(error)
     }
